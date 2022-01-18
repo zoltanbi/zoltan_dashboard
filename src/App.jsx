3c789ab5aedbaafdd95f9e35aab86d04391c56f5
Zoltanbi <zoltanbiro001@gmail.com>
2022-01-18 13:17:25 -0800

    setup react-router routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
 import './assets/libs/boxicons-2.1.1/css/boxicons.min.css'
 import './scss/App.scss'
 import Blank from './pages/Blank'
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import MainLayout from './layout/MainLayout';
+import Dashboard from './pages/Dashboard';
 
 function App() {
     return (
-        <div>
-            <Blank />
-        </div>
-    )
-}
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<MainLayout />}>
+                    <Route index element={<Dashboard />} />
+                    <Route path="orders" element={<Blank />} />
+                    <Route path="products" element={<Blank />} />
+                    <Route path="customers" element={<Blank />} />
+                    <Route path="settings" element={<Blank />} />
+                    <Route path="stats" element={<Blank />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    );
+};
 
-export default App
+export default App;
